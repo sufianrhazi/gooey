30c5c14ed1dbbedc982f108b55568a6f27a4c4f3
Sufian Rhazi <sufian@gmail.com>
2024-06-20 09:30:40 -0400

    Dumb optimization: avoid ArrayRenderNodes of 1 length

--- src/rendernode.tsx
+++ src/rendernode.tsx
@@ -560,6 +560,12 @@ export function ArrayRenderNode(
     children: RenderNode[],
     debugName?: string
 ): RenderNode {
+    if (children.length === 0) {
+        return emptyRenderNode;
+    }
+    if (children.length === 1) {
+        return children[0];
+    }
     return new RenderNode({}, children, debugName);
 }
 
