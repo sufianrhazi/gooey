d0e6386c73bd4fdbf5500db27cdc9083e1f6a547
Sufian Rhazi <sufian@gmail.com>
2025-04-18 10:12:51 -0400

    format

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
-import * as path from 'path';
 import * as fs from 'fs';
+import * as path from 'path';
+import { defineConfig } from 'vite';
 
 const demoFiles = fs
     .readdirSync(path.join('src/demos'))
     .filter((entry) => entry.endsWith('.html'))
-    .reduce((entryObj, entry) => {
-        entryObj[path.basename(entry, '.html')] = path.join('src/demos', entry);
-        return entryObj;
-    }, {} as Record<string, string>);
+    .reduce(
+        (entryObj, entry) => {
+            entryObj[path.basename(entry, '.html')] = path.join(
+                'src/demos',
+                entry
+            );
+            return entryObj;
+        },
+        {} as Record<string, string>
+    );
 
 export default defineConfig({
     esbuild: {
